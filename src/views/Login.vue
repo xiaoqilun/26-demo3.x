7fd9e0f26c4a06eda7a83526c4aa103fe726f879
alenok <aleno@163.com>
2019-02-18 12:59:43 +0800

    登陆静态页面

---
+++ src/views/Login.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="login">
+    <!-- model属性用于存放表单的数据对象 -->
+    <el-form :model="loginForm" class="container">
+      <el-form-item>
+        <img src="../assets/assets/avatar.jpg" alt="头像" class="avatar">
+      </el-form-item>
+      <el-form-item>
+         <el-input prefix-icon="myicon myicon-user" v-model="loginForm.username"></el-input>
+      </el-form-item>
+      <el-form-item>
+         <el-input prefix-icon="myicon myicon-key" v-model="loginForm.password" type="password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" class="login-btn">登陆</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .login {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background-color: #2f4050;
+
+  .container {
+    position: absolute;
+    left: 0;
+    right: 0;
+    width: 400px;
+    padding: 0px 40px 15px 40px;
+    margin: 200px auto;
+    background: white;
+    .avatar {
+      position: relative;
+      left: 50%;
+      width: 120px;
+      height: 120px;
+      margin-left: -60px;
+      margin-top: -60px;
+      box-sizing: border-box;
+      border-radius: 50%;
+      border: 10px solid #fff;
+      box-shadow: 0 1px 5px #ccc;
+      overflow: hidden;
+    }
+    .login-btn {
+      width: 100%;
+    }
+  }
+}
+</style>
